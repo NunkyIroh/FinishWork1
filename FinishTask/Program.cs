1a9bfe738ec7c537f8c5fd0fea43ebf4fb5e10d0
Uncle_Iroh <vadegtyarev@gmail.com>
2022-08-06 11:45:50 +0300

    Создал метода заполнения и вывода массива

---
+++ FinishTask/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.Write("Введите длинну массива: ");
+int sizeArr = int.Parse(Console.ReadLine());
+
+Console.Write("Заполните массив любыми символами: ");
+
+string[] CreateStringArray(int size)
+{
+    string[] arr = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("Массив [");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+
+    }
+}
+
+string[] array = CreateStringArray(sizeArr);
+PrintArray(array);
